c93832165abcbeb619ac959a3253c8ecadb00d07
Christine Augustine <blacchoy@gmail.com>
2020-03-30 10:43:18 -0700

    add code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
-    $(".btn").click(function(){
-        var name = $("input#name").val();
-        var person1 = $("input#person1").val();
-        var noun = $("input#noun").val();
-        console.log("noun");
-        $(".name").prepend(name);
-        $(".person1").text(person1);
-        $(".noun").text(noun);
-        
-        $("#story").show();
-        event.preventDefault();
-    });
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
 });
